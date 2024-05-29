64ee6ff034f97f425dd37a0f22fb0787e836f530
Natnael <107844070+Natcod@users.noreply.github.com>
2024-05-29 15:17:42 +0300

    chore: Update dependencies for Firebase and Glide libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.android.application")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -39,10 +40,11 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
     implementation ("com.squareup.picasso:picasso:2.71828")
-    implementation ("com.github.bumptech.glide:glide:4.12.0")
-    annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
-    implementation ("com.google.code.gson:gson:2.10.1")
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.16.0")
+    implementation ("com.google.code.gson:gson:2.11.0")
 
         implementation ("com.google.android.material:material:1.12.0")
         implementation ("androidx.viewpager2:viewpager2:1.1.0")
@@ -55,6 +57,20 @@ dependencies {
 
 
 
+
+
+    // Import the BoM for the Firebase platform
+    implementation(platform("com.google.firebase:firebase-bom:33.0.0"))
+
+
+    // Declare the dependency for the Cloud Firestore library
+
+    implementation("com.google.firebase:firebase-firestore")
+
+    implementation("com.google.firebase:firebase-analytics:22.0.0")
+    implementation("com.google.firebase:firebase-database:21.0.0")
+    implementation ("com.google.firebase:firebase-firestore:24.5.0")
+    implementation ("com.google.firebase:firebase-auth:21.0.1")
 
 
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,17 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.4.0" apply false
+    id("com.android.application") version "8.4.1" apply false
+    id("com.google.gms.google-services") version "4.3.10" apply false
 }
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath ("com.android.tools.build:gradle:7.0.4")
+        classpath("com.google.gms:google-services:4.4.1")
+    }
+}
+
