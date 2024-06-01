af5913cfaffbc26fc560e1514df972908fd792ed
Natnael <107844070+Natcod@users.noreply.github.com>
2024-06-01 20:09:21 +0300

    fake payment implementation

---
+++ app/src/main/java/com/example/tobiya_books/PaymentOptionsDialog.java
@@ -0,0 +1,24 @@
+//package com.example.tobiya_books;
+//
+//// PaymentOptionsDialog.java
+//import android.app.Dialog;
+//import android.content.Context;
+//import android.os.Bundle;
+//
+//import androidx.annotation.NonNull;
+//
+//public class PaymentOptionsDialog extends Dialog {
+//
+//    public PaymentOptionsDialog(@NonNull Context context) {
+//        super(context);
+//    }
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.payment_options_dialog);
+//
+//        // Initialize dialog UI elements and set up listeners
+//        // For example, add buttons and set onClickListeners
+//    }
+//}
